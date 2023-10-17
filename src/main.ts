1d4941a4dcfd7d4761daad360774c1b6edebc4d2
Benthan Vu <bxvu@ucsc.edu>
2023-10-16 22:01:41 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game 2";
+const gameName = "Colorful Canvas";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const drawingArea = document.createElement("canvas");
+const dimension = 256;
+drawingArea.width = dimension;
+drawingArea.height = dimension;
+
+const ctx = drawingArea.getContext("2d")!;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, dimension, dimension);
+
+app.append(drawingArea);
